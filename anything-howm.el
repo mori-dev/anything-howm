5803b2db10d0b376d5093aeb4822b9c0cc8563f4
mori-dev <mori.dev.asdf@gmail.com>
2012-03-28 08:23:49 +0900

    プレフィックスを変更

--- anything-howm.el
+++ anything-howm.el
@@ -5,7 +5,7 @@
 
 ;; Author: kitokitoki <mori.dev.asdf@gmail.com>
 ;; Keywords: anything, howm
-;; Prefix: anything-howm-
+;; Prefix: ah:
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -36,10 +36,10 @@
 
 ;; (require 'anything-howm)
 ;;
-;; (setq anything-howm-recent-menu-number-limit 600)
-;; (setq anything-howm-data-directory "/path/to/howm-directory")
-;; (global-set-key (kbd "C-2") 'anything-howm-menu-command)
-;; (global-set-key (kbd "C-3") 'anything-cached-howm-menu)
+;; (setq ah:recent-menu-number-limit 600)
+;; (setq ah:data-directory "/path/to/howm-directory")
+;; (global-set-key (kbd "C-2") 'ah:menu-command)
+;; (global-set-key (kbd "C-3") 'ah:cached-howm-menu)
 ;;
 ;; (defun anything-buffers ()
 ;;   (interactive)
@@ -61,27 +61,11 @@
 ;;         ...
 ;;         ))
 
-;;; Bug Report:
-;;
-;; If you have problem, send a bug report via M-x anything-howm-send-bug-report.
-;; The step is:
-;;  0) Setup mail in Emacs, the easiest way is:
-;;       (setq user-mail-address "your@mail.address")
-;;       (setq user-full-name "Your Full Name")
-;;       (setq smtpmail-smtp-server "your.smtp.server.jp")
-;;       (setq mail-user-agent 'message-user-agent)
-;;       (setq message-send-mail-function 'message-smtpmail-send-it)
-;;  1) Be sure to use the LATEST version of anything-howm.el.
-;;  2) Enable debugger. M-x toggle-debug-on-error or (setq debug-on-error t)
-;;  3) Use Lisp version instead of compiled one: (load "anything-howm.el")
-;;  4) Do it!
-;;  5) If you got an error, please do not close *Backtrace* buffer.
-;;  6) M-x anything-howm-send-bug-report and M-x insert-buffer *Backtrace*
-;;  7) Describe the bug using a precise recipe.
-;;  8) Type C-c C-c to send.
-;;  # If you are a Japanese, please write in Japanese:-)
-
 ;; Change Log
+;; 1.0.9: prefix を anything-howm- から ah: へ変更
+;; 1.0.8: 拡張子 .homn での判定処理を
+;;        howm-directory 以下の howm-mode かに変更
+;;        migemo をオプション化
 ;; 1.0.7: ファイル名ではなくタイトルを一覧表示する
 ;;        anything-c-source-buffers+-howm-title を追加
 ;; 1.0.6: 専用の anything-resume を作成
@@ -104,14 +88,15 @@
 (require 'howm)
 (require 'howm-menu)
 
-(defvar anything-howm-recent-menu-number-limit 10)
-(defvar anything-howm-persistent-action-buffer "*howm-tmp*")
-(defvar anything-howm-menu-buffer "*anything-howm-menu*")
-(defvar anything-howm-default-title "")
-(defvar anything-howm-data-directory "/path/to/howm-data-directory")
-(defvar anything-howm-use-migemo nil)
+(defvar ah:recent-menu-number-limit 10)
+(defvar ah:persistent-action-buffer "*howm-tmp*")
+(defvar ah:menu-buffer "*anything-howm-menu*")
+(defvar ah:default-title "")
+(defvar ah:data-directory "/path/to/howm-data-directory")
+(defvar ah:use-migemo nil)
 
-(defvar anything-howm-howm-full-path-directory (expand-file-name howm-directory))
+(defvar ah:howm-full-path-directory (expand-file-name howm-directory))
+
 
 ;;; Version
 
@@ -133,102 +118,102 @@ With prefix arg HERE, insert it at point."
     (init . (lambda ()
               (with-current-buffer (anything-candidate-buffer 'global)
                 (insert (mapconcat 'identity
-                                   (anything-howm-get-recent-title-list
-                                    (howm-recent-menu anything-howm-recent-menu-number-limit))
+                                   (ah:get-recent-title-list
+                                    (howm-recent-menu ah:recent-menu-number-limit))
                                    "\n")))))
     (candidates-in-buffer)
     (candidate-number-limit . 9999)
     (action .
-      (("Open howm file(s)" . anything-howm-find-files)
+      (("Open howm file(s)" . ah:find-files)
        ("Open howm file in other window" .
           (lambda (candidate)
             (find-file-other-window
-             (anything-howm-select-file-by-title candidate))))
+             (ah:select-file-by-title candidate))))
        ("Open howm file in other frame" .
           (lambda (candidate)
             (find-file-other-frame
-             (anything-howm-select-file-by-title candidate))))
+             (ah:select-file-by-title candidate))))
        ("Create new memo" .
           (lambda (template)
-            (anything-howm-create-new-memo "")))
+            (ah:create-new-memo "")))
        ("Create new memo on region" .
           (lambda (template)
-            (anything-howm-create-new-memo (anything-howm-set-selected-text))))
-       ("Delete file(s)" . anything-howm-delete-marked-files)))
-    (persistent-action . anything-howm-persistent-action)
+            (ah:create-new-memo (ah:set-selected-text))))
+       ("Delete file(s)" . ah:delete-marked-files)))
+    (persistent-action . ah:persistent-action)
     (cleanup .
       (lambda ()
-        (anything-aif (get-buffer anything-howm-persistent-action-buffer)
+        (anything-aif (get-buffer ah:persistent-action-buffer)
           (kill-buffer it))))))
 
-(when anything-howm-use-migemo
+(when ah:use-migemo
   (push '(migemo) anything-c-howm-recent))
 
-(defun anything-howm-persistent-action (candidate)
-  (let ((buffer (get-buffer-create anything-howm-persistent-action-buffer)))
+(defun ah:persistent-action (candidate)
+  (let ((buffer (get-buffer-create ah:persistent-action-buffer)))
       (with-current-buffer buffer
         (erase-buffer)
-        (insert-file-contents (anything-howm-select-file-by-title candidate))
+        (insert-file-contents (ah:select-file-by-title candidate))
         (goto-char (point-min)))
       (pop-to-buffer buffer)
       (howm-mode t)))
 
-(defun anything-howm-select-file-by-title (title)
-  (loop for recent-menu-x in (howm-recent-menu anything-howm-recent-menu-number-limit)
+(defun ah:select-file-by-title (title)
+  (loop for recent-menu-x in (howm-recent-menu ah:recent-menu-number-limit)
         for list-item-file  = (first recent-menu-x)
         for list-item-name  = (second recent-menu-x)
         if (string-equal title list-item-name)
           return list-item-file))
 
-(defun anything-howm-find-files (candidate)
+(defun ah:find-files (candidate)
   (anything-aif (anything-marked-candidates)
       (dolist (i it)
-        (find-file (anything-howm-select-file-by-title i)))
-    (find-file (anything-howm-select-file-by-title candidate))))
+        (find-file (ah:select-file-by-title i)))
+    (find-file (ah:select-file-by-title candidate))))
 
-(defun anything-howm-get-recent-title-list (recent-menu-list)
+(defun ah:get-recent-title-list (recent-menu-list)
   (loop for recent-menu-x in recent-menu-list
         for list-item-name  = (second recent-menu-x)
         collect list-item-name))
 
-(defun anything-howm-create-new-memo (text)
+(defun ah:create-new-memo (text)
   (let (memo-text str
         (cbuf (current-buffer)))
     (setq str text)
-    (howm-create-file-with-title anything-howm-default-title nil nil nil nil)
+    (howm-create-file-with-title ah:default-title nil nil nil nil)
     (save-excursion
       (goto-char (point-max))
       (insert str))
     (goto-char (point-min))
     (end-of-line)))
 
-(defun anything-howm-delete-marked-files (candidate)
+(defun ah:delete-marked-files (candidate)
   (anything-aif (anything-marked-candidates)
       (if (y-or-n-p (format "Delete *%s Files " (length it)))
           (progn
             (dolist (i it)
               (set-text-properties 0 (length i) nil i)
               (delete-file
-                (anything-howm-select-file-by-title i)))
+                (ah:select-file-by-title i)))
             (message "%s Files deleted" (length it)))
           (message "(No deletions performed)"))
     (set-text-properties 0 (length candidate) nil candidate)
     (if (y-or-n-p
-         (format "Really delete file `%s' " (anything-howm-select-file-by-title candidate)))
+         (format "Really delete file `%s' " (ah:select-file-by-title candidate)))
         (progn
           (delete-file
-            (anything-howm-select-file-by-title candidate))
+            (ah:select-file-by-title candidate))
           (message "1 file deleted"))
         (message "(No deletions performed)"))))
 
-(defun anything-howm-set-selected-text ()
+(defun ah:set-selected-text ()
   (if (region-active-p)
       (buffer-substring-no-properties (region-beginning) (region-end))
     ""))
 
-(defvar anything-howm-menu-list
-      '(("c [メモを作成]" . "(anything-howm-create-new-memo \"\")")
-        ("cr[リージョンからメモを作成]" . "(anything-howm-create-new-memo (anything-howm-set-selected-text))")
+(defvar ah:menu-list
+      '(("c [メモを作成]" . "(ah:create-new-memo \"\")")
+        ("cr[リージョンからメモを作成]" . "(ah:create-new-memo (ah:set-selected-text))")
         ("s [固定]" . "(howm-list-grep-fixed)")
         ("g [正規]" . "(howm-list-grep)")
         ("m [roma]" . "(howm-list-migemo)")
@@ -237,30 +222,31 @@ With prefix arg HERE, insert it at point."
 
 (defvar anything-c-source-howm-menu
   '((name . "メニュー")
-    (candidates . anything-howm-menu-list)
+    (candidates . ah:menu-list)
     (type . sexp)))
 
-(defun anything-cached-howm-menu ()
+(defun ah:cached-howm-menu ()
   (interactive)
-  (let ((anything-display-function 'anything-howm-display-buffer))
-    (if (get-buffer anything-howm-menu-buffer)
-        (anything-resume anything-howm-menu-buffer)
-      (anything-howm-menu-command))))
+  (let ((anything-display-function 'ah:display-buffer))
+    (if (get-buffer ah:menu-buffer)
+        (anything-resume ah:menu-buffer)
+      (ah:menu-command))))
 
-(defun anything-howm-menu-command ()
+
+(defun ah:menu-command ()
   (interactive)
-  (let ((anything-display-function 'anything-howm-display-buffer))
+  (let ((anything-display-function 'ah:display-buffer))
     (anything-other-buffer
      '(anything-c-source-howm-menu
        anything-c-howm-recent)
-     anything-howm-menu-buffer)))
+     ah:menu-buffer)))
 
-(defun anything-howm-resume ()
+(defun ah:resume ()
   (interactive)
-  (when (get-buffer anything-howm-menu-buffer)
-    (anything-resume anything-howm-menu-buffer)))
+  (when (get-buffer ah:menu-buffer)
+    (anything-resume ah:menu-buffer)))
 
-(defun anything-howm-display-buffer (buf)
+(defun ah:display-buffer (buf)
   "左右分割で表示する"
   (delete-other-windows)
   (split-window (selected-window) nil t)
@@ -274,7 +260,7 @@ With prefix arg HERE, insert it at point."
 (defvar anything-c-source-buffers+-howm-title
   '((name . "Buffers")
     (candidates . anything-c-buffer-list)
-    (real-to-display . anything-howm-title-real-to-display)
+    (real-to-display . ah:title-real-to-display)
     (type . buffer)
     (candidate-transformer
          anything-c-skip-current-buffer
@@ -289,23 +275,21 @@ With prefix arg HERE, insert it at point."
       (anything-c-buffers-persistent-kill candidate)
     (anything-c-switch-to-buffer candidate)))
 
-(defun anything-howm-title-real-to-display (file-name)
+(defun ah:title-real-to-display (file-name)
   (with-current-buffer (get-buffer file-name)
     (if (and howm-mode
-             (anything-howm-in-howm-dir-p file-name))
-             ;; (string-match anything-howm-howm-full-path-directory (buffer-file-name (get-buffer file-name))))
-      (anything-howm-title-get-title file-name)
+             (ah:in-howm-dir-p file-name))
+      (ah:title-get-title file-name)
     file-name)))
 
-(defun anything-howm-in-howm-dir-p (file-name)
-  (anything-howm-!!
-    (string-match anything-howm-howm-full-path-directory
-                  (buffer-file-name (get-buffer file-name)))))
+(defun ah:in-howm-dir-p (file-name)
+  (ah:!! (string-match ah:howm-full-path-directory
+                       (buffer-file-name (get-buffer file-name)))))
 
-(defun anything-howm-!! (arg)
+(defun ah:!! (arg)
   (not (not arg)))
 
-(defun anything-howm-title-get-title (buffer)
+(defun ah:title-get-title (buffer)
   (with-current-buffer buffer
     (let ((point (point-min)))
       (save-excursion
@@ -316,19 +300,20 @@ With prefix arg HERE, insert it at point."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; e.x, (global-set-key (kbd "C-c e") (anything-howm-fixed-term-command "emacs"))
-(defun anything-howm-fixed-term-command (initial)
+(defun ah:fixed-term-command (initial)
   (lexical-let ((initial initial))
     (lambda () (interactive) (anything 'anything-c-source-howm-recent initial))))
 
+
 ;; experimental code
-;(anything-howm-get-filename (list howm-directory))
-(defun anything-howm-get-filename (file-list)
+;(ah:get-filename (list howm-directory))
+(defun ah:get-filename (file-list)
     (loop for x in file-list
           with path-list = nil
           when (file-directory-p x)
             for path-list =
               (append
-                (anything-howm-get-filename
+                (ah:get-filename
                  (remove-if
                   (lambda(y) (string-match "\\.$\\|\\.svn" y))
                   (directory-files x t)))
@@ -340,33 +325,9 @@ With prefix arg HERE, insert it at point."
 
 (defvar anything-c-source-howm-contents-grep
   `((name . "anything-howm-contents-grep")
-    (grep-candidates . ,(anything-howm-get-filename (list howm-directory)))
+    (grep-candidates . ,(ah:get-filename (list howm-directory)))
     (header-name . (lambda (x) (concat x ": " anything-pattern)))
     (candidate-number-limit . 99999)))
 ;; (anything 'anything-c-source-howm-contents-grep)
-
-;;;; Bug report
-(defvar anything-howm-maintainer-mail-address
-  (concat "mor" "i.dev.asdf@gm" "ail.com"))
-(defvar anything-howm-bug-report-salutation
-  "Describe bug below, using a precise recipe.
-
-When I executed M-x ...
-
-How to send a bug report:
-  1) Be sure to use the LATEST version of anything-howm.el.
-  2) Enable debugger. M-x toggle-debug-on-error or (setq debug-on-error t)
-  3) Use Lisp version instead of compiled one: (load \"anything-howm.el\")
-  4) If you got an error, please paste *Backtrace* buffer.
-  5) Type C-c C-c to send.
-# If you are a Japanese, please write in Japanese:-)")
-(defun anything-howm-send-bug-report ()
-  (interactive)
-  (reporter-submit-bug-report
-   anything-howm-maintainer-mail-address
-   "anything-howm.el"
-   (apropos-internal "^eldoc-" 'boundp)
-   nil nil
-   anything-howm-bug-report-salutation))
 
 (provide 'anything-howm)
